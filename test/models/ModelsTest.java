1d7f8cb14429aa58518fa44a71e794b8d81b3d0c
mukulkedare82 <mukulgit82@gmail.com>
2016-05-14 14:42:57 +0530

    test data yaml file added plus test case

--- test/models/ModelsTest.java
+++ test/models/ModelsTest.java
@@ -64,4 +64,27 @@ public class ModelsTest extends WithApplication {
 								assertEquals(1, results.size());
 								assertEquals("Release next version", results.get(0).title);
 				}
+				@Test
+				public void fullTest() {
+								Ebean.save((List) Yaml.load("test-data.yml"));
+
+								// Count things
+								assertEquals(3, User.find.findRowCount());
+								assertEquals(7, Project.find.findRowCount());
+								assertEquals(5, Task.find.findRowCount());
+
+								// Try to authenticate as users
+								assertNotNull(User.authenticate("bob@example.com", "secret"));
+								assertNotNull(User.authenticate("jane@example.com", "secret"));
+								assertNull(User.authenticate("jeff@example.com", "badpassword"));
+								assertNull(User.authenticate("tom@example.com", "secret"));
+
+								// Find all Bob's projects
+								List<Project> bobsProjects = Project.findInvolving("bob@example.com");
+								assertEquals(5, bobsProjects.size());
+
+								// Find all Bob's todo tasks
+								List<Task> bobsTasks = Task.findTodoInvolving("bob@example.com");
+								assertEquals(4, bobsTasks.size());
+				}
 }
